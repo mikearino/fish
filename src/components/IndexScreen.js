417823cc627d29ff5e3b20ff406e96659440644b
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-10 18:10:17 -0800

    initial commit.

---
+++ src/components/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>This is the Index Screen</Text>
+    </View>
+  );
+};
+
+styles = StyleSheet.create({});
+
+export default IndexScreen;
